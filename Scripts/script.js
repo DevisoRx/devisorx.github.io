0935815c4b6148463f45d4e1a7f9c434b5202be7
DevisoRx <78548437+DevisoRx@users.noreply.github.com>
2025-02-19 13:48:58 +0000

    Add files via upload
    WebAPP PWGen

---
+++ Scripts/script.js
@@ -0,0 +1,12 @@
+function generatePassword() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?';
+    let password = '';
+    const length = document.getElementById('length').value;
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    document.getElementById('password').value = password;
+}
